fe821fd5822ee88cff5faf208ed1630eca73a4a9
marinasivakova <marinasivakova1799@gmail.com>
2024-06-18 23:03:21 +0300

    updating devs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
-import findTimerStatus from "./state/selectors/findTimerStatus";
-import TestApp from "./pages/TestApp/TestApp";
-import { start } from "./state/reducers/timer/timer";
-import Final from "./pages/Final/Final";
 import { Box, Button } from "@mui/material";
-import findTimerDeadline from "./state/selectors/findTimerDeadline";
 import { timer } from "./types/timer";
+import TestApp from "./pages/TestApp/TestApp";
+import Final from "./pages/Final/Final";
+import { start } from "./state/reducers/timer/timer";
+import findTimerDeadline from "./state/selectors/findTimerDeadline";
+import findTimerStatus from "./state/selectors/findTimerStatus";
 import getQuestions from "./state/selectors/getQuestions";
 
 function App() {
